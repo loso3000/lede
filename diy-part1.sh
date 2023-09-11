bab0521ea82d1b0cb389528243a99c7791f07c23
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-09-11 10:51:59 +0800

    a

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,4 @@
 #=================================================
 cp -Rf ./sc/.github/tmp/* .
 chmod +x openwrt/*.sh
-mv openwrt/package/pd/luci-theme-catjs   openwrt/package/luci-theme-kucat
 bash diy-part1.sh
