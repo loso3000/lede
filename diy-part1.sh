9288a8064ae759f9c8bb74cecd1074254058489a
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-09-11 22:42:57 +0800

    x

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#=================================================
+echo "=================== 1 cp -Rf ../.github/tmp/* .========================="
 cp -Rf ../lede/.github/tmp/* .  ||true
 ls ../lede/.github/tmp
-echo '-------------------------------'
+echo '------------ 2 cp -Rf ../.github/tmp/* .-------------------'
 cp -Rf ../.github/tmp/* .  ||true
 ls ./.github/tmp/
-echo '-------------------------------'
+echo '------------- 3 ls------------------'
 ls
 chmod +x diy.sh
 bash diy.sh
