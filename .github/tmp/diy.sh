3e82c8204f1b78eb93febaf7ae9983ed25255b8d
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-09-14 03:15:28 +0800

    a

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -2,9 +2,9 @@
 #=================================================
 # Description: Build OpenWrt using GitHub Actions
 WORKDIR=/workdir
-HOSTNAME=EzOpWrt
-IPADDRESS=192.168.10.1
-OP_THEME=kucat
+HOSTNAME="EzOpWrt"
+IPADDRESS="192.168.10.1"
+OP_THEME="kucat"
 SSID=Sirpdboy
 ENCRYPTION=psk2
 KEY=123456
@@ -313,18 +313,19 @@ for sh_file in `ls ${GITHUB_WORKSPACE}/openwrt/common/*.sh`;do
     source $sh_file amd64
 done
 
+
 # echo '默认开启 Irqbalance'
 #ver1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
 export VER1="$(grep "KERNEL_PATCHVER:="  ./target/linux/x86/Makefile | cut -d = -f 2)"
 #date1=`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`'-Ipv6-Super-Vip'
 ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-5.4 | cut -d . -f 3`
+export date1=`TZ=UTC-8 date +%Y.%m.%d -d +"12"hour`'-Super-'${VER1}'.'${ver54}''
 export date1=`$(TZ=UTC-8 date +%Y.%m.%d -d +"12"hour)`"-Super-${VER1}.${ver54}"
 #sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/$(shell TZ=UTC-8 date +%Y%m%d -d +12hour)-Ipv6-Super-Vip-5.10-/g' include/image.mk
 #sed -i 's/$(VERSION_DIST_SANITIZED)-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)/20230601-Ipv6-Super-Vip-5.10-/g' include/image.mk
 echo ${date1}'_by_Sirpdboy' > ./package/base-files/files/etc/ezopenwrt_version
-echo ${HOSTNAME} ${date1}'_by_Sirpdboy' >> ./package/base-files/files/etc/banner
+echo "${HOSTNAME} ${date1}_by_Sirpdboy" >> ./package/base-files/files/etc/banner
 echo '---------------------------------' >> ./package/base-files/files/etc/banner
-
 # rename_version=`cat files/etc/ezopenwrt_version`
 
 ./scripts/feeds update -i
