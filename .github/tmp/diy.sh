955f1f49096e92a8b1b375b5758957780df0e30b
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-15 13:16:51 +0800

    x

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -25,12 +25,15 @@ mv -rf  ./package/emortal2/default-settings   ./package/emortal/default-settings
 mv -rf  ./package/emortal2/automount   ./package/emortal/automount
 mv -rf  ./package/emortal2/autosamba   ./package/emortal/autosamba
 
-
-#if [ ${CONFIG_S} = "Mini" || ${CONFIG_S} = "Super" ] ; then
+case "${CONFIG_S}" in
+Super|Mini|Vip-Mini|Vip-Mini)
 sed -i 's/nas/services/g' ./feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
-#fi
-
+#samba4
+sed -i 's/nas/services/g' ./feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
+;;
+esac
 sed -i 's/status/services/g' ./feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+
 #samba4
 sed -i 's/nas/services/g' ./feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
 
