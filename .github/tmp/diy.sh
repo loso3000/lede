6ec7e0f028c01b863e4cb41203f96befcf9afb64
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-11-24 23:03:11 +0800

    m

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -150,7 +150,7 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
-rm -rf package/mosdns/mosdns
+rm -rf ./feeds/package/mosdns
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
