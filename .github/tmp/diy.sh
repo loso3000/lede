464d8db944ff1542df8249546ffbebdbe379a72e
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-01-18 11:34:55 +0800

    x

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -184,34 +184,14 @@ rm -rf ./feeds/luci/applications/luci-app-aria2  package/feeds/packages/luci-app
 
 
 # Passwall
-rm -rf ./feeds/packages/net/pdnsd-alt
-#rm -rf ./feeds/packages/net/shadowsocks-libev
-rm -rf ./feeds/packages/net/xray-core
-# rm -rf ./feeds/packages/net/kcptun
-rm -rf ./feeds/packages/net/brook
-rm -rf ./feeds/packages/net/chinadns-ng
-rm -rf ./feeds/packages/net/dns2socks
-rm -rf ./feeds/packages/net/hysteria
-rm -rf ./feeds/packages/net/ipt2socks
-rm -rf ./feeds/packages/net/dns2tcp
-rm -rf ./feeds/packages/net/microsocks
-rm -rf ./feeds/packages/net/naiveproxy
-rm -rf ./feeds/packages/net/shadowsocks-rust
-rm -rf ./feeds/packages/net/simple-obfs
-rm -rf ./feeds/packages/net/ssocks
-rm -rf ./feeds/packages/net/tcping
-rm -rf ./feeds/packages/net/v2ray*
-rm -rf ./feeds/packages/net/xray*
-rm -rf ./feeds/packages/net/trojan*
-rm -rf ./feeds/packages/net/hysteria
 
 #ssr
 #git_url "
 # 	https://github.com/xiaorouji/openwrt-passwall-packages
 #	https://github.com/fw876/helloworld
 #"
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone https://github.com/fw876/helloworld package/helloworld
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+# git clone https://github.com/fw876/helloworld package/helloworld
 
 rm -rf package/openwrt-passwall/xray-core
 rm -rf package/openwrt-passwall/xray-plugin
@@ -230,7 +210,7 @@ rm -rf ./feeds/luci/applications/luci-app-ssr-plus  package/feeds/packages/luci-
 # rm -rf ./feeds/luci/applications/luci-app-passwall  package/feeds/packages/luci-app-passwall
 
 
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 
 #git_exp xiaorouji/openwrt-passwall2 luci-app-passwall2
