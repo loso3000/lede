fb8d67f0cb80c168ec6dd290caafc1efdd9d625a
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-01-28 14:12:04 +0800

    v

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -79,7 +79,7 @@ rm -rf ./feeds/packages/net/trojan-plus
 rm -rf package/feeds/packages/naiveproxy
 rm -rf ./feeds/packages/net/naiveproxy
 
-rm -rf ./feeds/luci/applications/luci-app-vssr
+# rm -rf ./feeds/luci/applications/luci-app-vssr
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus  package/feeds/packages/luci-app-ssr-plus
 
 git clone https://github.com/loso3000/other ./package/other
