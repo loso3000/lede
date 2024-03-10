e9432acc70e91cc32377c468038a99b43c6944df
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-03-11 03:22:15 +0800

    x

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -79,7 +79,7 @@ rm -rf ./package/ssr/trojan-plus
 rm -rf ./package/ssr/naiveproxy
 # rm -rf ./package/ssr/v2ray-plugin
 # rm -rf ./package/ssr/v2ray-core
-rm -rf ./package/ssr/pdnsd
+# rm -rf ./package/ssr/pdnsd
 
 git clone https://github.com/loso3000/other ./package/other
 git clone https://github.com/sirpdboy/netspeedtest ./package/netspeedtest
