5c8e15cc3bbc469d6b4f68a12efa2f9ae220507b
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2024-07-31 22:59:17 +0800

    a

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -32,12 +32,12 @@ export mirror=raw.githubusercontent.com/coolsnowwolf/lede/master
 # grep HASH include/kernel-5.4 | awk -F'HASH-' '{print $2}' | awk '{print $1}' | md5sum | awk '{print $1}' > .vermagic
 
 # alist
-rm -rf ./feeds/packages/net/alist
-rm -rf  ./feeds/luci/applications/luci-app-alist
+# rm -rf ./feeds/packages/net/alist
+# rm -rf  ./feeds/luci/applications/luci-app-alist
 # alist
-git clone https://$github/sbwml/luci-app-alist package/alist
+# git clone https://$github/sbwml/luci-app-alist package/alist
 # git clone -b v3.32.0 --depth 1 https://$github/sbwml/luci-app-alist package/alist
-sed -i '/config.json/a\ rm -rf \/var\/run\/alist.sock' package/alist/alist/files/alist.init
+# sed -i '/config.json/a\ rm -rf \/var\/run\/alist.sock' package/alist/alist/files/alist.init
 # rm -rf ./package/alist/alist
 
 # sed -i 's/网络存储/存储/g' ./package/alist/luci-app-alist/po/*/alist.po
