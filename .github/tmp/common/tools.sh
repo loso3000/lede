53e27e1b79938f94bdb3000d7155ab2deffae768
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-02-23 17:29:51 +0800

    x

--- .github/tmp/common/tools.sh
+++ .github/tmp/common/tools.sh
@@ -7,18 +7,7 @@
 # Author: SuLingGG
 # Blog: https://mlapp.cn
 #=================================================
-if [ $1 == amd64 ] ;then
-BASE_FILES=${GITHUB_WORKSPACE}/openwrt/package/base-files/files
-singbox_version="1.8.8"
-hysteria_version="2.3.0"
-# wget --quiet --no-check-certificate -P /tmp https://github.com/SagerNet/sing-box/releases/download/v${singbox_version}/sing-box-${singbox_version}-linux-amd64.tar.gz
-wget --quiet --no-check-certificate -P /tmp https://github.com/apernet/hysteria/releases/download/app%2Fv${hysteria_version}/hysteria-linux-amd64
-mkdir  -p ${BASE_FILES}/usr/bin
-tar -xvzf /tmp/sing-box-${singbox_version}-linux-amd64.tar.gz -C /tmp
-#cp -r /tmp/sing-box-${singbox_version}-linux-amd64/sing-box ${BASE_FILES}/usr/bin
-cp -r /tmp/hysteria-linux-amd64 ${BASE_FILES}/usr/bin/hysteria
-chmod 777 ${BASE_FILES}/usr/bin/sing-box ${BASE_FILES}/usr/bin/hysteria
-fi
+
 mkdir -p files/root
 cp  -rf ./patch/z.zshrc ./files/root/.zshrc
 pushd files/root
@@ -32,4 +21,22 @@ git clone https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+popd
+
+## opkg ##
+PLATFORM=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '"' '{print $2}')
+TARGET=$(cat .config | grep CONFIG_TARGET_BOARD | awk -F '"' '{print $2}')
+SUBTARGET=$(cat .config |  grep CONFIG_TARGET | sed -n 2p | awk -F '=' '{print $1}')
+
+mkdir -p files/etc/opkg
+pushd files/etc/opkg
+cat <<-EOF > "distfeeds.conf"
+src/gz ezopwrt_core https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/targets/x86/64/packages
+src/gz ezopwrt_base https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/x86_64/base
+src/gz ezopwrt_luci https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/x86_64/luci
+src/gz ezopwrt_packages https://downloads.immortalwrt.org/releases/24.10-SNAPSHOTT/packages/x86_64/packages
+src/gz ezopwrt_routing https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/x86_64/routing
+src/gz ezopwrt_telephony https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/x86_64/telephony
+EOF
+cp distfeeds.conf distfeeds.conf.bak
 popd
